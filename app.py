8736a4c878d9c3b69729eed2996368b6697235d1
Cristian Favaro <cristianfavaroo@gmail.com>
2023-03-15 21:49:46 -0300

    retirando o teste

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 menu = '''
-<a href="/">Pagina Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a> | <a href="/teste">Teste</a>
+<a href="/">Pagina Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a> 
 <br>
 '''
 
