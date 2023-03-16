1858456509687fb3c7b704cc916084e1e3404a61
Cristian Favaro <cristianfavaroo@gmail.com>
2023-03-15 21:41:51 -0300

    pequeno ajuste para entrar as novas páginas

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
+
 from flask import Flask
 
 app = Flask(__name__)
 
+menu = '''
+<a href="/">Pagina Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a> | <a href="/teste">Teste</a>
+<br>
+'''
+
 @app.route("/")
-def hello_world():
-   return "Olá, mundo!"
+def index():
+   return menu + "Meu nome é Cristian, como vai?"
+
+@app.route("/sobre")
+def sobre():
+   return menu + "Aqui vai o conteúdo da página Sobre. Posso por um html depois"
+
+@app.route("/contato")
+def contato():
+   return menu + "Aqui vai o conteúdo da página Contato. Entrar html depois"   
